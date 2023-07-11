1e8a7ee7fcf37bf0b2f7c410dac82a592c0f8904
azatkhaliyllin <khaliyllin56@gmail.com>
2023-07-11 17:08:04 +0400

    Arrays101ExploreCard (3)

---
+++ src/main/java/com/azatkhaliullin/leetcode/TasksFromArrays101ExploreCard/SquaresOfASortedArray.java
@@ -0,0 +1,21 @@
+package com.azatkhaliullin.leetcode.TasksFromArrays101ExploreCard;
+
+import java.util.Arrays;
+
+public class SquaresOfASortedArray {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {-4, -1, 0, 3, 10};
+        System.out.println(Arrays.toString(solution.sortedSquares(nums)));
+    }
+
+    static class Solution {
+        public int[] sortedSquares(int[] nums) {
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = (int) Math.pow(nums[i], 2);
+            }
+            return Arrays.stream(nums).sorted().toArray();
+        }
+    }
+
+}
